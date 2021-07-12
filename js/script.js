a5e8f56dfc236270ca28a493f7f9e9a78934e7d5
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2021-07-12 13:28:30 -0300

    Fixing bugs

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
-// Todos
-
-// CSS 
-
-// (X) Add Update my projects in the portfolio repository;
-// (X) Update the portfolio page;
-// (X) Make the Form work;
-
-
-
-// JavaScript
-
-// (V) Show the navbar 
-// (V) Toggle the class with "display: none" after the transition is over
-// (X) Make the script work\
-
-   
-
        /******************* Selectors *******************/
-//         const body = document.body;
-// const topNavLinks = document.querySelectorAll("#nav-list li a");
 const menuCheckbox = document.querySelector("input#menu-checkbox");
 const menuIcons = document.querySelector("label.menu-icons");
 const form = document.getElementById("contact-form");
@@ -29,28 +9,6 @@ menuCheckbox.addEventListener('change', toggleIcon);
 // menuIcons.addEventListener('click', toggleIcon);
 
         /******************* Functions *******************/
-
-// Active link function 
-
-// for (var i = 0; i < topNavAnchors.length; i++) {  
-
-//         topNavAnchors[i].addEventListener("click", function(e) {  
-
-//                 // Get all the elements that have the class "active"   
-//                 const current = document.querySelectorAll(".active");  
-        
-//                 if (current.length > 0) {
-//                         for(let i = 0; i < current.length; i++) {
-//                                 // Remove the "active" class from all elements 
-//                                 current[i].classList.remove('active');
-//                         }
-//                 }
-//                 this.className += " active";
-//          });
-// }
-
-
-
 
 function toggleIcon() {
         const burgerIcon = document.querySelector('#burger-icon');
@@ -73,49 +31,4 @@ function toggleIcon() {
 
 
 // Page transition SWUP library
-const swup = new Swup();
-
-
-
-// Form function 
-// async function handleSubmit(event) {
-//   event.preventDefault();
-//   const status = document.getElementById("my-form-status");
-//   const data = new FormData(event.target);
-//   fetch(event.target.action, {
-//         method: form.method,
-//         body: data,
-//         headers: {
-//                 'Accept': 'application/json'
-//     }
-//   }).then(response => {
-//         status.classList.add('success');
-//         status.innerHTML = "E-mail enviado!";
-//         form.reset()
-//   }).catch(error => {
-//         status.classList.add('error');
-//         status.innerHTML = "Oops! Houve um problema.";
-//   });
-// }
-// form.addEventListener("submit", handleSubmit);
-
-// var form = document.getElementById("my-form");
-    
-// async function handleSubmit(event) {
-//   event.preventDefault();
-//   var status = document.getElementById("my-form-status");
-//   var data = new FormData(event.target);
-//   fetch(event.target.action, {
-//     method: form.method,
-//     body: data,
-//     headers: {
-//         'Accept': 'application/json'
-//     }
-//   }).then(response => {
-//     status.innerHTML = "Thanks for your submission!";
-//     form.reset()
-//   }).catch(error => {
-//     status.innerHTML = "Oops! There was a problem submitting your form"
-//   });
-// }
-// form.addEventListener("submit", handleSubmit)
+// const swup = new Swup();
